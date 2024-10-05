da9718e1e2657b9f1d52b683565a34cfbda63a95
Jubayer husain <jubayerhusai360@gmail.com>
2024-10-05 12:54:52 +0600

    show all function done

--- script/phone.js
+++ script/phone.js
@@ -1,27 +1,37 @@
 const searchHandler = () => {
     const loadSpinner = document.getElementById('spinner');
     loadSpinner.classList.remove('hidden')
-    console.log("Search Btn Cliked");
+    // console.log("Search Btn Cliked");
+    const searchField = document.getElementById('search-field').value;
     setTimeout(() => {
-        loadAllPhone();
+        loadAllPhone(false, searchField);
     }, 3000);
 }
-const loadAllPhone = async () => {
-    console.log('Hey jubayer 3 second done')
+const loadAllPhone = async (status, searchInput) => {
+    console.log(searchInput);
     const loadSpinner = document.getElementById('spinner');
     loadSpinner.classList.add('hidden')
 
-    const respons = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`);
+    const respons = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchInput?searchInput:"iphone"}`);
     const data = await respons.json();
-    displayAllPhone(data.data);
+    if (status == true) {
+        displayAllPhone(data.data);
+    } else {
+        displayAllPhone(data.data.slice(0, 6));
+
+    }
 }
 
 
 const displayAllPhone = (allphoine) => {
     console.log('Display All phone Called')
-    console.log(allphoine)
+    // console.log(allphoine)
+}
+
+const showAllPhoneBtn = () => {
+    loadAllPhone(true)
 }
 
 
 // globaly call function 
-loadAllPhone()
+loadAllPhone(false, "iphone")
