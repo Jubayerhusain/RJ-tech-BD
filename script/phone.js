bd84979896cc5126c7d1ecef8bc3401c0c5f65fa
Jubayer husain <jubayerhusai360@gmail.com>
2024-10-05 11:28:37 +0600

    loadAllPhone from API

--- script/phone.js
+++ script/phone.js
@@ -1,9 +1,3 @@
-const loadAllPhone = () =>{
-    console.log('Hey jubayer 3 second done')
-    const loadSpinner = document.getElementById('spinner');
-    loadSpinner.classList.add('hidden')
-}
-
 const searchHandler = () => {
     const loadSpinner = document.getElementById('spinner');
     loadSpinner.classList.remove('hidden')
@@ -12,3 +6,22 @@ const searchHandler = () => {
         loadAllPhone();
     }, 3000);
 }
+const loadAllPhone = async () => {
+    console.log('Hey jubayer 3 second done')
+    const loadSpinner = document.getElementById('spinner');
+    loadSpinner.classList.add('hidden')
+
+    const respons = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`);
+    const data = await respons.json();
+    displayAllPhone(data.data);
+}
+
+
+const displayAllPhone = (allphoine) => {
+    console.log('Display All phone Called')
+    console.log(allphoine)
+}
+
+
+// globaly call function 
+loadAllPhone()
