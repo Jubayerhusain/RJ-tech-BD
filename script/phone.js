37929d92c6b680c94d67788158d02859b3036232
Jubayer husain <jubayerhusai360@gmail.com>
2024-10-05 11:01:51 +0600

    loadAll phone with spinner done

--- script/phone.js
+++ script/phone.js
@@ -1,1 +1,14 @@
-console.log('Hey Dear Welcone to new API');
+const loadAllPhone = () =>{
+    console.log('Hey jubayer 3 second done')
+    const loadSpinner = document.getElementById('spinner');
+    loadSpinner.classList.add('hidden')
+}
+
+const searchHandler = () => {
+    const loadSpinner = document.getElementById('spinner');
+    loadSpinner.classList.remove('hidden')
+    console.log("Search Btn Cliked");
+    setTimeout(() => {
+        loadAllPhone();
+    }, 3000);
+}
